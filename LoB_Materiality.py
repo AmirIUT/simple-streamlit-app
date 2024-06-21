4c68070df9df09a75484dc82e09ecb26ff3cf106
IUTProDE <amir.aminia@outlook.com>
2024-06-21 15:40:54 +0200

    Update LoB_Materiality.py

--- LoB_Materiality.py
+++ LoB_Materiality.py
@@ -232,8 +232,8 @@ def section_2_investment_activities(session_state):
         for i in range(5):
             gb_cols = st.columns([0.1, 1, 1])
             gb_cols[0].write(f"**{i+1}**")
-            country = gb_cols[1].selectbox("Top Countries - Government Bond", options=countries, key=f"country_{i}", help=f"Select the country for government bond {i + 1}")
-            exposure = gb_cols[2].selectbox("Exposure Materiality", options=["Low", "Medium", "High", "Not relevant/No Exposure"], key=f"exposure_gb_{i}", help=f"Select the exposure level for government bond {i + 1}")
+            country = gb_cols[1].selectbox(options=countries, key=f"country_{i}", help=f"Select the country for government bond {i + 1}")
+            exposure = gb_cols[2].selectbox(options=["Low", "Medium", "High", "Not relevant/No Exposure"], key=f"exposure_gb_{i}", help=f"Select the exposure level for government bond {i + 1}")
 
 
 
