a70ed1dc163faf6c14e99a6f428357bf5060b927
IUTProDE <amir.aminia@outlook.com>
2024-06-21 15:03:44 +0200

    Update LoB_Materiality.py

--- LoB_Materiality.py
+++ LoB_Materiality.py
@@ -230,10 +230,10 @@ def section_2_investment_activities(session_state):
         
         # Rows for the top 5 countries
         for i in range(5):
-            gb_cols = st.columns([0.1, 1, 1])
-            gb_cols[0].write(f"**{i + 1}**")
-            country = gb_cols[1].selectbox("", options=countries, key=f"country_{i}", help=f"Select the country for government bond {i + 1}", label_visibility="collapsed")
-            exposure = gb_cols[2].selectbox("", options=["Low", "Medium", "High", "Not relevant/No Exposure"], key=f"exposure_{i}", help=f"Select the exposure level for government bond {i + 1}", label_visibility="collapsed")
+    gb_cols = st.columns([0.1, 1, 1])
+    gb_cols[0].write(f"**{i+1}**")
+    country = gb_cols[1].selectbox(f"country_{i}", options=countries, key=f"country_{i}", help=f"Select the country for government bond {i + 1}")
+    exposure = gb_cols[2].selectbox(f"exposure_{i}", options=["Low", "Medium", "High", "Not relevant/No Exposure"], key=f"exposure_gb_{i}", help=f"Select the exposure level for government bond {i + 1}")
 
 
 
